24b5e856608d189f3e769c5a5bcb9d72076d0bfc
YukJeongGeun <wjdrms5815@gmail.com>
2022-04-18 15:38:53 +0900

    #18

--- src/main/java/com/SkhuMentoring/dto/Mentee.java
+++ src/main/java/com/SkhuMentoring/dto/Mentee.java
@@ -1,26 +1,6 @@
 package com.SkhuMentoring.dto;
 
 import lombok.Data;
-
-import java.util.Date;
-
-@Data
-public class Mentee {
-    private int bno;
-    private String userId;
-    private String userStudentNum;
-    private String mentorWho;
-    private String department;
-    private String subjectName;
-    private Date startDate;
-    private Date endDate;
-    private Date regDate;
-    private String hopeDay;
-    private String introduce;
-}
-package com.SkhuMentoring.dto;
-
-import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
--- src/main/java/com/SkhuMentoring/dto/Mentor.java
+++ src/main/java/com/SkhuMentoring/dto/Mentor.java
@@ -1,26 +1,6 @@
 package com.SkhuMentoring.dto;
 
 import lombok.Data;
-
-import java.util.Date;
-
-@Data
-public class Mentor {
-    private int bno;
-    private String userId;
-    private String userStudentNum;
-    private int menteeCount;
-    private String department;
-    private String subjectName;
-    private Date startDate;
-    private Date endDate;
-    private Date regDate;
-    private String hopeDay;
-    private String introduce;
-}
-package com.SkhuMentoring.dto;
-
-import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -33,6 +13,7 @@ public class Mentor {
     private String department;
     private String subjectName;
     private String subject;
+    private String menteeCount;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
